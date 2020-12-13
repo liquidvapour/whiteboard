0d4099fea7bce9d33451ac3a12d0d46ff8fb4cbc
liquidvapour <liquidvapour@gmail.com>
2020-12-13 16:36:31 +0000

    add axios

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = [{
+    target: 'web',
     mode: 'development',
     entry: {
       main: './src/ui/index.js',      
