221dd29a18e3518f41b5ee21cd8834c3f14a5c7b
liquidvapour <liquidvapour@gmail.com>
2020-12-20 04:09:10 +0000

    allow navigation direct to board

--- src/ui/index.js
+++ src/ui/index.js
@@ -12,8 +12,19 @@ const navToBoard = (boardName, formHolder) => {
         .catch(e => console.log(`error: ${e}`));
 };
 
+const uiBoardPathMatcher = new RegExp('/ui/(?<boardId>[0-9a-z-]+)');
+
 const startUp = (document) => {
+
     const holder = document.createElement("div", { id: "appRoot"});
+
+    const boardIdMatch = window.location.pathname.match(uiBoardPathMatcher);
+
+    if (boardIdMatch && boardIdMatch.groups && boardIdMatch.groups.boardId) {
+        navToBoard(boardIdMatch.groups.boardId, holder);
+        return false;
+    }
+
 
     document.body.append(holder);
 
