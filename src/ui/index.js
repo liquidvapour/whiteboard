c33cf8f1ad678103653e663dcf6cf23dd96291e2
liquidvapour <liquidvapour@gmail.com>
2020-12-21 20:30:32 +0000

    more tweaks

--- src/ui/index.js
+++ src/ui/index.js
@@ -21,19 +21,17 @@ const uiBoardPathMatcher = new RegExp('/ui/(?<boardId>[0-9a-z-]+)');
 const startUp = (document) => {
 
     const holder = document.createElement("div", { id: "appRoot"});
+    document.body.append(holder);
 
     const boardIdMatch = window.location.pathname.match(uiBoardPathMatcher);
 
     if (boardIdMatch && boardIdMatch.groups && boardIdMatch.groups.boardId) {
         navToBoard(boardIdMatch.groups.boardId, holder);
-        return false;
+    } else {
+        ReactDOM.render(
+            <BoardForm onSubmit={boardId => navToBoard(boardId, holder)}/>, 
+            holder);
     }
-
-    document.body.append(holder);
-
-    ReactDOM.render(
-        <BoardForm onSubmit={boardId => navToBoard(boardId, holder)}/>, 
-        holder);
 };
 
 startUp(document);
