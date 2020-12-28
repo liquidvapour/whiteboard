5557040fd758f198088f2bfbb38381dbc6f40c08
liquidvapour <liquidvapour@gmail.com>
2020-12-28 17:49:15 +0000

    an unknown boardname is just a new board

--- src/ui/server.js
+++ src/ui/server.js
@@ -4,10 +4,16 @@ const root = '/back/board';
 console.log(`app root url: `)
 
 export const populateStrokes = async (boardName) => {
-    const response = await axios.get(`${root}/${boardName}`)
+    console.log(`populateStrokes ${boardName}`);
+    const response = await axios.get(
+        `${root}/${boardName}`, {
+            validateStatus: function (status) {
+            return status < 500; // Resolve only if the status code is less than 500
+            }
+        });
 
     console.log(`response ${JSON.stringify(response.data)}`);
-    return response.data.strokes;
+    return response.data.strokes || [];
 };
 
 export const postStroke = async (boardName, stroke) => {
