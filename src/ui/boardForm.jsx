c215550fd982177928c537844277ef170fc8fc1d
liquidvapour <liquidvapour@gmail.com>
2021-03-06 23:56:08 +0000

    simplify form

--- src/ui/boardForm.jsx
+++ src/ui/boardForm.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-export class BoardForm extends Component {
+export class BoardForm extends React.Component {
     constructor(props) {
         super(props);
+        this.onTxtBoardChange = this.onTxtBoardChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
         this.state = {
             boardId: props.boardId || ''
         };
@@ -22,13 +24,13 @@ export class BoardForm extends Component {
 
     render() {
         return (
-            <Form onSubmit={e => this.onSubmit(e)}>
+            <Form onSubmit={this.onSubmit}>
                 <Form.Group>
                     <Form.Label>board id</Form.Label>
-                    <Form.Control 
-                        type='text' 
+                    <Form.Control
+                        type='text'
                         value={this.state.boardId}
-                        onChange={e => this.onTxtBoardChange(e)} >
+                        onChange={this.onTxtBoardChange} >
                     </Form.Control>
                     <Button variant="primary" type="submit">Submit</Button>
                 </Form.Group>
