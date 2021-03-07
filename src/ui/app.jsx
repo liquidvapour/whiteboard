e352b3e576e4595f2a91b547e9df8380f25d9743
liquidvapour <liquidvapour@gmail.com>
2021-03-07 02:48:33 +0000

    Fix compile error

--- src/ui/app.jsx
+++ src/ui/app.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import Container from 'react-bootstrap/Container';
-
 
 export const App = ({children}) => (
     <div className="d-flex flex-wrap justify-content-center position-absolute w-100 h-100 align-items-center align-content-center">
